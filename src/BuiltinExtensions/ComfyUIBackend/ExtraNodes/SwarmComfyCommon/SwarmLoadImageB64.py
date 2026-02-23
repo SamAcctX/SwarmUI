13fcf136ca148c1c6f68d94570621557c0a12b2b
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2026-02-22 19:23:25 -0800

    node SwarmLoadAudioB64

--- src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyCommon/SwarmLoadImageB64.py
+++ src/BuiltinExtensions/ComfyUIBackend/ExtraNodes/SwarmComfyCommon/SwarmLoadImageB64.py
@@ -2,6 +2,10 @@ from PIL import Image, ImageOps
 import numpy as np
 import torch, base64, io
 from comfy_api.input_impl import VideoFromFile
+try:
+    from comfy_extras.nodes_audio import load as raw_audio_load
+except:
+    print("Error: Nodes_Audio failed to import, Swarm will not be able to load audio files.")
 
 def b64_to_img_and_mask(image_base64):
     imageData = base64.b64decode(image_base64)
@@ -62,7 +66,28 @@ class SwarmLoadVideoB64:
         video_bytes = io.BytesIO(video_data)
         return (VideoFromFile(video_bytes), )
 
+class SwarmLoadAudioB64:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "audio_base64": ("STRING", {"multiline": True})
+            }
+        }
+    CATEGORY = "SwarmUI/images"
+    RETURN_TYPES = ("AUDIO",)
+    FUNCTION = "load_audio_b64"
+    DESCRIPTION = "Loads an audio from a base64 string. Works like a regular LoadAudio node, but with input format designed to be easier to use through automated calls, including SwarmUI with custom workflows."
+
+    def load_audio_b64(self, audio_base64):
+        audio_data = base64.b64decode(audio_base64)
+        audio_bytes = io.BytesIO(audio_data)
+        waveform, sample_rate = raw_audio_load(audio_bytes)
+        audio = {"waveform": waveform.unsqueeze(0), "sample_rate": sample_rate}
+        return (audio, )
+
 NODE_CLASS_MAPPINGS = {
     "SwarmLoadImageB64": SwarmLoadImageB64,
     "SwarmLoadVideoB64": SwarmLoadVideoB64,
+    "SwarmLoadAudioB64": SwarmLoadAudioB64,
 }
